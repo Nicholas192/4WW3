6d7627b32c321bf5e760529f7e4dc6ceb4919c3c
unknown <nicholasmigliore999@gmail.com>
2019-12-02 13:04:58 -0500

    cssfixP1

--- Assigment 1/Assigment 1/logout.php
+++ Assigment 1/Assigment 1/logout.php
@@ -4,6 +4,7 @@
 
     include('header.php');
     include('footer.php');
-
+// i added the footer because i changed the header to include a div opening, which gives a background colour for the entire page
+// the end of the div is in the footer so should i include footer.php here?
     auth_logout();
     header('Location: index.php');
