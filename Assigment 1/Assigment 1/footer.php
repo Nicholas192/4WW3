66b19108268d46228ec4beb1e5e587f5b2dc98da
EC2 Default User <ec2-user@ip-172-31-32-189.ec2.internal>
2019-12-04 02:17:42 +0000

    header and footer updates

--- Assigment 1/Assigment 1/footer.php
+++ Assigment 1/Assigment 1/footer.php
@@ -3,6 +3,6 @@
         <h1>About Our Team</h1>
         <p>we will put contact information here</p>
     </div>
-    
+</div>     
     </body>
 </html>
--- Assigment 1/Assigment 1/header.php
+++ Assigment 1/Assigment 1/header.php
@@ -2,17 +2,59 @@
 
 <html lang="en">
 	<head>
-		<meta charset="UTF-8">
+
+<meta charset="UTF-8">
 
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
     	<meta http-equiv="X-UA-Compatible" content="ie=edge">
-		<title>Homepage Restaurant Site</title>
+
+		<link rel="icon" href="images/favicon.png">
+		<!-- third-generation iPad with high-resolution Retina display: -->
+		<link rel="apple-touch-icon-precomposed" sizes="144x144" href="images/restaurant-finder.png">
+		<!-- iPhone with high-resolution Retina display: -->
+		<link rel="apple-touch-icon-precomposed" sizes="114x114" href="images/restaurant-finder.png">
+		<!-- first- and second-generation iPad: -->
+		<link rel="apple-touch-icon-precomposed" sizes="72x72" href="images/restaurant-finder.png">
+		<!-- non-Retina iPhone, iPod Touch, and Android 2.1+ devices: -->
+		<link rel="apple-touch-icon-precomposed" href="images/favicon.png">
+
+
+		<!--  Below are the names of the tabs, which appear at the top of the browser   -->
+		<!--  We might want to include the name of our restaurant in these, especially the homepage    -->
+		<!-- also, we should find a way to include the user's search in the search results tab's title and
+		also include the name of the restaurant in the individual object page. Add more cases if there is more pages that need them -->
+		<?php 
+			if ($page=='login') {echo '<title>Login</title>';} 
+			elseif ($page=='logout') {echo '<title>Logout</title>';} 
+			elseif ($page=='register') {echo '<title>Register</title>';}
+			elseif ($page=='search-form') {echo '<title>Search Restaurants</title>';} 
+			elseif ($page=='object-submission-page') {echo '<title>Submit Restaurant</title>';}
+			elseif ($page=='results-page') {echo '<title></title>';} 
+			else {echo '<title>Homepage</title>';}
+		?>
+
+		<meta name="author" content="Nicholas Migliore, Edward Johnson, David Carrie">
+		<meta name="description" content="The Restaurant Finder aims to help people who are hungry to find a
+		place to eat that suits their desires. The app will provide users with a map of restaurant locations
+		along with a description of what each one serves and a user rating and review system.">
 
 		<!-- links the css file to the html file -->
 		<link rel="stylesheet" href="css/style.css">
 	</head>
 
 	<body>
+		<?php
+			if ($page=='login') {echo '<div class=body-blue>';} 
+			elseif ($page=='logout') {echo '<div class=body-blue>';} 
+			elseif ($page=='register') {echo '<div class=body-green>';}
+			elseif ($page=='search-form') {echo '<div class=body-yellow>';} 
+			elseif ($page=='object-submission-page') {echo '<div class=body-orange>';}
+			else {echo '<div class=body-white>';}
+		?>
+		<div class="header">
+			<h1>Restaurant Finder</h1>
+			<img class="symbol" src="images/restaurant-finder.png" alt="Relevant textual alternative to the image"/>
+		</div>
 		<div class="top-navigation">
 
             <?php if (is_logged_in()) { ?>
@@ -22,6 +64,7 @@
                 </a>
             <?php } ?>
 			<a <?php if ($page=='index') echo 'class="active"'; ?> id="home" href="index.php">Homepage</a>
+
 
 			<?php if (is_logged_in()) {?>
 				<a <?php if ($page=='object-submission-page') echo 'class="active"'; ?> id="submit" href="object-submission-page.php">Submission</a>
